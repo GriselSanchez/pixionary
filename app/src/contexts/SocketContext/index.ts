dda6c92b499335d320e122868e324a7536c2d1ce
Grisel Sanchez <griselalmasanchez@gmail.com>
2020-12-27 17:00:20 -0300

    test commit

--- app/src/contexts/SocketContext/index.ts
+++ app/src/contexts/SocketContext/index.ts
@@ -2,6 +2,7 @@ import React from "react";
 import io from "socket.io-client";
 
 const API_URL = process.env.REACT_APP_API_URL || "http://192.168.1.43:3001";
+
 const socket = io(API_URL);
 
 const SocketContext = React.createContext<SocketIOClient.Socket>(socket);
